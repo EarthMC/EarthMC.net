12e3342529996d872a93dce686eccce9b0266170
CloakedEagle <cloakedeagle.cobalt@gmail.com>
2019-05-08 01:12:23 +0530

    Added responsive for nav bar

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var x=document.getElementById("toggleMenu");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
