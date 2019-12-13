b45ef4705dadf5e6811f4b76ce548e69edb3c789
Eagle <swagata@swagatadas.com>
2019-12-14 03:20:54 +0530

    Introduced node for development
    - npm added
    - changed the directory structure
    - added Litematica as allowed mod
    - updated towny commands page link

---
+++ webpack.dev.config.js
@@ -0,0 +1,67 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader",
+                        options: {
+                            minimize: false,
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpe?g|JPE?G|gif)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[path][name].[ext]",
+                            esModule: false
+                        }
+                    }    
+                ]
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "src/index.html",
+            filename: "index.html",
+            favicon: 'src/favicon.ico'
+        }),
+        new HtmlWebPackPlugin({
+            template: "src/rules.html",
+            filename: "rules.html",
+            favicon: 'src/favicon.ico'
+        }),
+        new HtmlWebPackPlugin({
+            template: "src/rules_cn.html",
+            filename: "rules_cn.html",
+            favicon: 'src/favicon.ico'
+        }),
+        new HtmlWebPackPlugin({
+            template: "src/faq.html",
+            filename: "faq.html",
+            favicon: 'src/favicon.ico'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+}
